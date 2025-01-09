a4e93b27cf368d025513495007e611c60278c310
Tran Tuyen <61371510+tuyen1204@users.noreply.github.com>
2025-01-09 15:54:54 +0700

    [feat]: class TodoDetail

---
+++ ziichat_todo/lib/screens/folder/folder_detail.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:ziichat_todo/constants.dart';
+import 'package:ziichat_todo/screens/buttons/add_item.dart';
+import 'folder_item.dart';
+
+class TodoDetail extends StatelessWidget {
+  const TodoDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final folder = ModalRoute.of(context)!.settings.arguments as FolderItem;
+
+    return Scaffold(
+      backgroundColor: primaryColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_new, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.white),
+            onPressed: () => {},
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: Colors.white,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child:
+                            Icon(Icons.ballot, size: 32, color: primaryColor),
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      folder.title,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Text(
+                      folder.isDefault == false
+                          ? '${folder.task.toString()} task'
+                          : ' task',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(16),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "Late",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w600),
+                          ),
+                          ListView.builder(
+                            itemCount: 4,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [Text("data")],
+                              );
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: AddItemButton(),
+    );
+  }
+}
